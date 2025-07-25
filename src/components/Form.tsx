20bf567746749602a09914d5812690c3f7f92d49
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-25 19:47:30 -0300

    updated password field

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -28,7 +28,47 @@ export default function Form() {
         />
 
         {errors.name && <p style={{ color: "red"}}>{errors.name.message}</p>}
-        <button type="submit">Submit</button>
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email" 
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+               message: "Invalid email address",
+           },
+
+        })} 
+        id="email"
+        placeholder="Email"   
+       />
+
+       {errors.email && (
+          <div style={{ color: "red" }}>{errors.email.message}</div>
+       )}
+
+       <div>
+         <input 
+           type="password"
+           {...register("password", {
+             minLength: {
+               value: 0,
+               message: "Password must be at least 8 characters",
+             },
+           })} 
+           placeholder="Password"
+         />
+         
+         {errors.password && (
+          <div style={{ color: "red" }}>{errors.password.message}</div>
+         )}
+
+         <button type="submit">Submit</button>
+
+       </div>
       </div>
     </form>
   );
