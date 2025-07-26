d6889cdba6dbad1865ee73e8f7ea029e293a07d3
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-25 21:24:31 -0300

    added last name field

--- src/components/AdvancedForm.tsx
+++ src/components/AdvancedForm.tsx
@@ -1,5 +1,57 @@
+import {useForm, SubmitHandler} from "react-hook-form"
+import "../style.css"
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+  completeLocation: string;
+}
+
+const onSubmit: SubmitHandler<FormData> = (data) => {
+   console.log(data);
+}
+
+
 export default function AdvancedForm() {
+  const {
+   register,
+   handleSubmit,
+   formState: {errors},
+  } = useForm<FormData>();
+
   return (
-    <div>AdvancedForm</div>
+    <div className="form-container">
+      <h2>Registration Form</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+         <div>
+            <label htmlFor="firstName">First Name</label>
+            <input 
+              type="text" 
+              id="firstName" 
+              {...register("firstName", {required: "First Name is required"})} 
+             />
+
+          {errors.firstName && <p>{errors.firstName.message}</p>}
+         </div>
+
+         <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input 
+              type="text" 
+              id="lastName" 
+              {...register("lastName", {required: "Last Name is Required"})}
+            />
+
+           {errors.lastName && <p>{errors.lastName.message}</p>}
+         </div>
+      </form>
+
+    </div>
   )
 }
